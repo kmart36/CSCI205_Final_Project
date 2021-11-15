78dc182cd197058b23c9c99c1b968919b9b7a865
Douglas Russo <dmr034@bucknell.edu>
2021-11-15 10:32:07 -0500

    Allows the scene to be displayed through IntelliJ.

---
+++ src/main/java/CheckersFXMLMain.java
@@ -0,0 +1,32 @@
+import CheckersModel.Piece;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CheckersFXMLMain extends Application {
+
+    /** Create the players */
+    private Piece player1;
+    private Piece player2;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Load in the FXML file and obtain the root node of the scene graph.(Code from lab11)
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/checkersfxml.fxml"));
+        Parent root = loader.load();
+
+        // Set up our stage(Code from lab11)
+        primaryStage.setTitle("Play Checkers!");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
