54592f8d1af8ddf65569f2ef08ae3202ff7b1cf8
Katy Martinson <kam071@bucknell.edu>
2021-11-18 12:05:57 -0500

    Skeleton documentation

---
+++ src/main/java/CheckersModel/CheckersModel.java
@@ -0,0 +1,23 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2021
+ * Instructor: Prof. Brian King
+ *
+ * Name: Katy Martinson
+ * Section: 9:30am
+ * Date: 11/12/2021
+ * Time: 10:11 AM
+ *
+ * Project: csci205_final_project
+ * Package: CheckersModel
+ * Class: CheckersModel
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package CheckersModel;
+
+public class CheckersModel {
+
+}
